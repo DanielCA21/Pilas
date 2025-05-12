c38f14a0b8de96d52ef72a7088c7f10d62446847
Daniel Cristancho <dasancristal91@gmail.com>
2025-05-12 08:01:11 -0500

    Add files via upload

---
+++ Pilas_Daniel_Cristancho/index.php
@@ -0,0 +1,22 @@
+<?php
+        
+
+$pila = array();
+
+array_push ($pila,"nevera");
+array_push ($pila,"cafetera");
+array_push ($pila,"televisor");
+array_push ($pila,"ventilador");
+array_push ($pila,"horno");
+array_push ($pila,"licuadora");
+array_push ($pila,"lavadora");
+array_push ($pila,"alexa");
+array_push ($pila,"aspiradora");
+
+
+    $instrumentos = array_pop ($pila);
+
+    echo "el ultimo elemento ingresado a la pila es:".$electrodomésticos; 
+
+?>
+
